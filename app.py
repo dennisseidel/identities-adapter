25087b382cff521262b22e0844911e0691cbf490
Dennis Seidel <den.seidel@gmail.com>
2018-03-09 00:06:58 +0100

    feat: add CORS

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from connexion.resolver import RestyResolver
 import connexion
+from flask_cors import CORS
+
 
 
 if __name__ == '__main__':
     app = connexion.FlaskApp(__name__, specification_dir='swagger/')
-    # Check Configuring Flask-Cache section for more details
+    
+    # add CORS support
+    CORS(app.app)
+    
     app.add_api('swagger.yaml', resolver=RestyResolver('api'))
     app.run(port=9090)
