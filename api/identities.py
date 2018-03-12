c11a6a6b86cc567db9225a1f49328505276cd3af
Dennis Seidel <den.seidel@gmail.com>
2018-03-12 23:23:30 +0100

    chore: refactor code
    closes #14

--- api/identities.py
+++ api/identities.py
@@ -14,24 +14,27 @@ auth0_client_id= urllib.parse.quote_plus(os.environ['auth_client_id'])
 auth0_client_secret= urllib.parse.quote_plus(os.environ['auth_client_secret'])
 apigee_client_id= os.environ['apigee_client_id']
 apigee_client_secret= os.environ['apigee_client_secret']
+auth0_endpoint = os.environ['auth0_endpoint'] # https://d10l.eu.auth0.com
+apigee_management_endpoint = os.environ['apigee_management_endpoint'] # https://api.enterprise.apigee.com/v1/organizations/denseidel-trial
+apigee_auth_endpoint = os.environ['apigee_auth_endpoint'] # https://login.apigee.com/oauth/token
 
-def post(identityid, client):
-  # TODO check if token is in cache and only call auth0 if not available or expired 
-  # TODO put this into it's own function
-  token_endpoint = "https://d10l.eu.auth0.com/oauth/token"
-  token_request_body = {
+def get_idp_access_token():
+  # TODO check if token is in cache and only call auth0 if not available or expired
+  endpoint= "%s/oauth/token" % (auth0_endpoint)
+  request_body = {
     "client_id": auth0_client_id,
     "client_secret": auth0_client_secret, 
-    "audience": "https://d10l.eu.auth0.com/api/v2/",
+    "audience": "%s/api/v2/" % (auth0_endpoint),
     "grant_type": "client_credentials"
     }
-  token_headers = { 'content-type': "application/json" }
-  response = requests.post(token_endpoint, json=token_request_body, headers=token_headers)
+  headers = { 'content-type': "application/json" }
+  response = requests.post(endpoint, json=request_body, headers=headers)
   jsonData = response.json()
   access_token = jsonData['access_token']
   # TODO cache this token
-  # function create client in auth0 
-  endpoint = "https://d10l.eu.auth0.com/api/v2/clients"
+  return access_token
+
+def create_client_in_idp(access_token, client):
   request_body = {
     "name": client['client_name'],
     "description": client['client_description'],
@@ -39,46 +42,53 @@ def post(identityid, client):
     "allowed_logout_urls": client['allowed_logout_urls']
   }
   headers = {"Authorization":"Bearer " + access_token}
-  auth0_client_created = requests.post(endpoint,json=request_body,headers=headers).json()
+  client = requests.post("%s/api/v2/clients" % (auth0_endpoint),json=request_body,headers=headers).json()
+  return client
+
+def get_apigw_access_token():
   # TODO check if apigee token is in cache otherwise get new token
-  apigee_token_endpoint = "https://login.apigee.com/oauth/token"
-  apigee_token_request_body = {
+  endpoint = apigee_auth_endpoint
+  request_body = {
     'username': apigee_client_id,
     'password': apigee_client_secret, 
     'grant_type': 'password'
   }
-  apigee_token_headers = {
+  headers = {
     'Authorization': 'Basic ZWRnZWNsaTplZGdlY2xpc2VjcmV0'
   }
-  r = requests.post(apigee_token_endpoint, data=apigee_token_request_body, headers=apigee_token_headers)
+  r = requests.post(endpoint, data=request_body, headers=headers)
   jsonData = r.json()
-  apigee_access_token=jsonData['access_token']
+  access_token=jsonData['access_token']
+  return access_token
+
+def create_client_in_apigw(access_token, client):
   # get the email from the profiledb and add it to the request 
-  apigee_endpoint="https://api.enterprise.apigee.com/v1/organizations/denseidel-trial/developers/den.seidel@gmail.com/apps"
-  apigee_add_app_request_body= {
-    "name": client['client_name'],
-    #"callbackUrl" : "https://url-for-3-legged-oauth/"
+  endpoint="%s/developers/den.seidel@gmail.com/apps" % (apigee_management_endpoint)
+  request_body= {
+    "name": client['name'],
   }
-  apigee_add_app_request_headers = {
-    "Authorization": "Bearer " + apigee_access_token,
+  headers = {
+    "Authorization": "Bearer " + access_token,
     "Content-Type": "application/json",
   }
-  r = requests.post(apigee_endpoint, json=apigee_add_app_request_body, headers=apigee_add_app_request_headers)
+  r = requests.post(endpoint, json=request_body, headers=headers)
   # update the clientid and client secret with the one recieved from the IDM
-  apigee_update_client_credentials_endpoint = 'https://api.enterprise.apigee.com/v1/organizations/denseidel-trial/developers/%s/apps/%s/keys/create' % ('den.seidel@gmail.com', client['client_name'])
-  apigee_update_client_credentials_request_body = {
-    "consumerKey": auth0_client_created['client_id'], 
-    "consumerSecret": auth0_client_created['client_secret']
+  endpoint = '%s/developers/%s/apps/%s/keys/create' % (apigee_management_endpoint, 'den.seidel@gmail.com', client['client_name'])
+  request_body = {
+    "consumerKey": client['client_id'], 
+    "consumerSecret": client['client_secret']
   }
-  apigee_updated_client_credentials = requests.post(apigee_update_client_credentials_endpoint,json=apigee_update_client_credentials_request_body,headers=apigee_add_app_request_headers)
+  requests.post(endpoint,json=request_body,headers=headers)
+
+def save_client_to_profiledb(client, identityid):
   # save in a key value store under the identityid the array of clients including everything needed to display in the portal
   identities = mdb_client['identities']
   identity = identities.identity
   identity_data = { '_id': identityid }
   client_data = {
-    'client_id': auth0_client_created['client_id'],
-    'client_name': auth0_client_created['name'],
-    'client_description': auth0_client_created['description'],
+    'client_id': client['client_id'],
+    'client_name': client['name'],
+    'client_description': client['description'],
     'date_created': datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
   }
   result = identity.update_one(identity_data, { '$push': { 'clients': client_data}}, upsert=True)
@@ -88,10 +98,17 @@ def post(identityid, client):
     "client_description": client_data['client_description'],
     "date_created": client_data['date_created']
   }
+  return client
+
+def post(identityid, client):
+  idp_access_token = get_idp_access_token()
+  idp_created_client = create_client_in_idp(idp_access_token,client)
+  apigw_access_token = get_apigw_access_token()
+  create_client_in_apigw(apigw_access_token, idp_created_client)
+  client = save_client_to_profiledb(idp_created_client, identityid)
   return client, 201
 
 def get(identityid):
-  print(identityid)
   # get the data for the identityid from the profile db
   identities = mdb_client['identities']
   identity = identities.identity
@@ -101,7 +118,6 @@ def get(identityid):
   }
   for client in result["clients"]:
     clients['clients'].append(client) 
-    #{'client_id': 'yecR7Dprg7tbip4P3d4gAbcnW6leQcqG', 'client_name': 'test', 'client_description': 'test', 'date_created': '2018-03-11T19:41:14'}
   return clients, 201
 
 def patch(identityid, identity):
