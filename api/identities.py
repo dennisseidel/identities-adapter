06e43cdd697c254fe63bdad6ebbade2a46b3d561
Dennis Seidel <den.seidel@gmail.com>
2018-04-06 09:01:30 +0200

    chore: remove old log message

--- api/identities.py
+++ api/identities.py
@@ -25,11 +25,9 @@ def get_idp_access_token():
     "audience": "%s/api/v2/" % (auth0_endpoint),
     "grant_type": "client_credentials"
     }
-  print(request_body)
   headers = { 'content-type': "application/json" }
   response = requests.post(endpoint, json=request_body, headers=headers)
   jsonData = response.json()
-  print(jsonData)
   access_token = jsonData['access_token']
   # TODO cache this token
   return access_token
